af6dd4537694c52e5b746e3ff5c7637f057b13bb
Vlad Safin <djutsu17@gmail.com>
2021-07-28 16:27:29 +0300

    project start

---
+++ src/services/imagesApi.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const apiKey = "21359619-d33abf781fc12db63121b07d3"
+
+axios.defaults.baseURL = "https://pixabay.com/api/"
+
+const fetchData = ({
+  searchQuery = "",
+  currentPage = 1,
+  perPage,
+}) => {
+  return axios
+    .get(
+      `/?q=${searchQuery}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}`
+    )
+    .then((response) => response.data.hits)
+}
+
+export default { fetchData }
